e6c1c899b4a4f7358aa39f1d452359a52d6ea572
tushart29 <tushar29t@gmail.com>
2023-09-07 09:44:05 -0700

    seperated API calls

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -7,14 +7,23 @@ export const NewsContextProvider = (props) => {
   const [data, setData] = useState();
   const apiKey = "d3a68d3a93a54948a016a1553bc4d20c";
 
-  const { category, search } = props; // Access the category prop
+  const { category, search } = props; // Access the category and search props
 
   useEffect(() => {
-    axios
-      .get(`https://newsapi.org/v2/top-headlines?q=${search}&country=us&category=${category}&apiKey=${apiKey}`)
-      .then((response) => setData(response.data))
-      .catch((error) => console.log(error));
-  }, [category, search]); // Add category as a dependency
+    if (search) { // Check if search has a value
+      axios
+        .get(`https://newsapi.org/v2/everything?q=${search}&from=2023-08-07&sortBy=publishedAt&apiKey=${apiKey}`)
+        .then((response) => setData(response.data))
+        .catch((error) => console.log(error));
+      console.log(search);
+    } else {
+      // Call a different axios.get request when search is empty
+      axios
+        .get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`)
+        .then((response) => setData(response.data))
+        .catch((error) => console.log(error));
+    }
+  }, [category, search]); // Add category and search as dependencies
 
   return (
     <NewsContext.Provider value={{ data }}>
