7b2343a82f0e22a24b6d50b7a3af3e067e4e6a69
joshuaenoch <joshuaechow@gmail.com>
2023-09-07 15:11:21 -0700

    trash

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -10,20 +10,10 @@ export const NewsContextProvider = (props) => {
   const { category, search } = props; // Access the category and search props
 
   useEffect(() => {
-    if (search) { // Check if search has a value
-      axios
-        .get(`https://newsapi.org/v2/everything?q=${search}&pageSize=100&apiKey=${apiKey}`)
-        .then((response) => setData(response.data))
-        .catch((error) => console.log(error));
-
-    } else {
-      // Call a different axios.get request when search is empty
-
-      axios
-        .get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=100&apiKey=${apiKey}`)
-        .then((response) => setData(response.data))
-        .catch((error) => console.log(error));
-    }
+    axios
+      .get(`https://newsapi.org/v2/top-headlines?q=${search}&country=us&category=${category}&pageSize=100&apiKey=${apiKey}`)
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
   }, [category, search]); // Add category and search as dependencies
 
   return (
